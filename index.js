4f59717e3a1f1842950be4cedcfffe27ee5fb7dd
Darina44 <johndoe@email.com>
2022-11-20 22:37:07 +0300

    lessons

---
+++ index.js
@@ -0,0 +1,19 @@
+let errors = [];
+function checkvalidity(input) {
+    let validity = input.validity;
+    if (validity.valueMissing) {errors.push('Поле ' + input.placeholder + ' не заполнено'); }
+    if (validity.patternMismatch) {errors.push('Неверный формат заполнения'); }
+    if (validity.rangeOverFlow) {let max = getAttributeValue(input, 'max'); errors.push('Максимальное значение не может быть больше чем ' + max); }
+    if (validity.rangeUnderFlow) {let min = getAttributeValue(input, 'min'); errors.push('Минимальное значение не может быть меньше чем ' + min); }
+} 
+
+function checkAll() {
+    errors = [];
+    let inputs = document.querySelectorAll("input");
+
+    for (let input of inputs) {
+        checkValidity(input);
+    }
+
+    document.getElementById('errorsInfo').innerHTML=errors.join('. <br>');
+}
