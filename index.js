cdf3ee82ae45bee90362697bb2ab9f8b84bfd11a
Darina44 <johndoe@email.com>
2022-11-21 21:53:13 +0300

    lessons

--- index.js
+++ index.js
@@ -18,3 +18,25 @@
 //     document.getElementById('errorsInfo').innerHTML=errors.join('. <br>');
 // }
 
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    let name = localStorage.getItem('name');
+    if (name != null) {
+        document.getElementById("author").value = name;
+    }
+});
+
+function sendMessage(author, comment) {
+    document.getElementById("chat").innerHTML += `<span class='author'>${author}: </span><span>${comment}</span><br>`;
+}
+
+function checkMessage() {
+    let author = document.getElementById("author").value;
+    let comment = document.getElementById("comment").value;
+
+    if (localStorage.getItem('name') == null) {
+        localStorage.setItem('name', author);
+    }
+
+    sendMessage(author, comment);
+}
